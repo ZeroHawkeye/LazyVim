914059ec0fc9237a148dc61bab6e58ea9bda9c5d
virus <166997982@qq.com>
2024-05-31 14:11:50 +0800

    update config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,27 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  vim.o.guifont = "JetBrains Mono:h14"
+  vim.g.neovide_refresh_rate = 120
+  vim.g.neovide_refresh_rate_idle = 5
+  vim.g.neovide_fullscreen = true
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_cursor_animate_command_line = true
+  vim.g.neovide_cursor_animate_in_insert_mode = true
 
+  vim.g.neovide_floating_blur_amount_x = 2.0
+  vim.g.neovide_floating_blur_amount_y = 2.0
+
+  vim.g.neovide_floating_shadow = true
+  vim.g.neovide_floating_z_height = 10
+  vim.g.neovide_light_angle_degrees = 45
+  vim.g.neovide_light_radius = 5
+end
 opt.relativenumber = false
 -- 保存自动格式化
-vim.g.autoformat = false
+vim.g.autoformat = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.showmode = true
--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,56 +1,5 @@
 return {
   {
-    "Exafunction/codeium.vim",
-    config = function()
-      -- Change '<C-g>' here to any keycode you like.
-      vim.keymap.set("i", "<C-g>", function()
-        return vim.fn["codeium#Accept"]()
-      end, { expr = true, silent = true })
-      vim.keymap.set("i", "<c-;>", function()
-        return vim.fn["codeium#CycleCompletions"](1)
-      end, { expr = true, silent = true })
-      vim.keymap.set("i", "<c-,>", function()
-        return vim.fn["codeium#CycleCompletions"](-1)
-      end, { expr = true, silent = true })
-      vim.keymap.set("i", "<c-x>", function()
-        return vim.fn["codeium#Clear"]()
-      end, { expr = true, silent = true })
-    end,
-  },
-  {
-    "0x00-ketsu/autosave.nvim",
-    config = function()
-      require("autosave").setup({
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-        enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
-        trigger_events = { "InsertLeave", "TextChanged" }, -- vim events that trigger auto-save. See :h events
-        -- function that determines whether to save the current buffer or not
-        -- return true: if buffer is ok to be saved
-        -- return false: if it's not ok to be saved
-        condition = function(buf)
-          local fn = vim.fn
-          local utils = require("auto-save.utils.data")
-
-          if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
-            return true -- met condition(s), can save
-          end
-          return false -- can't save
-        end,
-        write_all_buffers = false, -- write all buffers when the current one meets `condition`
-        debounce_delay = 135, -- saves the file at most every `debounce_delay` milliseconds
-        callbacks = { -- functions to be executed at different intervals
-          enabling = nil, -- ran when enabling auto-save
-          disabling = nil, -- ran when disabling auto-save
-          before_asserting_save = nil, -- ran before checking `condition`
-          before_saving = nil, -- ran before doing the actual save
-          after_saving = nil, -- ran after doing the actual save
-        },
-      })
-    end,
-  },
-  {
     "telescope.nvim",
     dependencies = {
       -- project management
@@ -318,11 +267,11 @@ return {
     "nvimdev/dashboard-nvim",
     event = "VimEnter",
     opts = function()
-      local logo = [[__________                   
-\____    /___________  ____  
-  /     // __ \_  __ \/  _ \ 
+      local logo = [[__________
+\____    /___________  ____
+  /     // __ \_  __ \/  _ \
  /     /\  ___/|  | \(  <_> )
-/_______ \___  >__|   \____/ 
+/_______ \___  >__|   \____/
         \/   \/              ]]
 
       logo = string.rep("\n", 8) .. logo .. "\n\n"
